55aedbf769437071f38c9de16f7badb972e357c3
Chris Chernesky <chris@tinderglow.com>
2023-03-22 12:16:54 -0400

    Fix calendar icon for bootstrap

--- app/helpers/user_announcements/admin_helper.rb
+++ app/helpers/user_announcements/admin_helper.rb
@@ -1,5 +1,5 @@
 module UserAnnouncements::AdminHelper
-  
+
   def _ua_active_model_errors(model)
     return unless model.errors.present?
 
@@ -10,17 +10,17 @@ module UserAnnouncements::AdminHelper
       end
     end
   end
-  
+
   def ua_table_tag(options={})
     content_tag(:table, options.merge(ua_table_attrs)) do
       yield
     end
   end
-  
+
   def ua_boolean_display(boolean)
     ( boolean ? '&#10004;' : '&nbsp;' ).html_safe
   end
-  
+
   def ua_datetime_p(f, method)
     if ua_bootstrap_datetime_picker?
       ua_datetime_p_bootstrap(f, method)
@@ -28,7 +28,7 @@ module UserAnnouncements::AdminHelper
       ua_datetime_p_non_bootstrap(f, method)
     end
   end
-  
+
   private
 
   def ua_table_attrs
@@ -38,14 +38,14 @@ module UserAnnouncements::AdminHelper
       {class: 'ua-table non-bootstrap'}
     end
   end
-  
+
   def ua_datetime_p_bootstrap(f, method)
     value = f.object.send(method).try(:strftime, "%Y-%m-%d %H:%M")
     %(<p class="input-append date ua-datetimepicker" style: "display:block !important">
       #{f.label(method)}
       #{f.text_field(method, value: value, data: {format: "yyyy-MM-dd hh:mm"})}
       <span class="add-on">
-        <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+        <i data-time-icon="icon-time" data-date-icon="fa fa-calendar">
         </i>
       </span>
     </p>).html_safe
@@ -53,11 +53,11 @@ module UserAnnouncements::AdminHelper
 
   def ua_datetime_p_non_bootstrap(f, method)
     content_tag(:p, class: 'datetime-select') do
-      f.label(method) + 
-      ua_br +      
+      f.label(method) +
+      ua_br +
       f.datetime_select(method)
     end
   end
-  
-  
+
+
 end
